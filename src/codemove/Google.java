f766710b801acc1c86a53d3b8481496b1760fe8f
sowjanya <sowjanyasarath2016@gmail.com>
2020-12-11 14:52:07 -0500

    first commit

---
+++ src/codemove/Google.java
@@ -0,0 +1,20 @@
+package codemove;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver",
+				  "C:\\Personal\\Selenium\\Browsers\\Chrome\\chromedriver_win32 (86)\\chromedriver.exe");
+		
+		ChromeDriver  driver = new ChromeDriver();
+		          driver.get("http://www.google.com");
+		          driver.quit();
+		
+
+	}
+
+}
